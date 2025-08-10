0022a8ecb2b3bb1af845dd74d485d3ee4f56614f
Mustafa Impis <maveralabs@gmail..com>
2025-08-10 22:40:58 +0200

    Add initial dish data to the database

---
+++ scripts/db.js
@@ -0,0 +1,23 @@
+let myDishes = [
+  {
+    id: 1,
+    name: "Pizza Margherita",
+    description: "Pizza mit Tomatensauce, Mozzarella und frischem Basilikum",
+    price: 8.99,
+    image: "./assets/img/pizza-margherita.jpg"
+  },
+  {
+    id: 2,
+    name: "Spaghetti Carbonara",
+    description: "Pasta mit cremiger Sauce, Pancetta und Parmesan",
+    price: 10.99,
+    image: "./assets/img/spaghetti-carbonara.jpg"
+  },
+  {
+    id: 3,
+    name: "Caesar Salad",
+    description: "Frischer Römersalat mit Caesar-Dressing und Croutons",
+    price: 7.99,
+    image: "./assets/img/caesar-salad.jpg"
+  }
+];

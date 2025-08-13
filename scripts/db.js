20834950b61c56084bf11f008390ccadef8bd82d
Mustafa Impis <maveralabs@gmail..com>
2025-08-13 22:07:35 +0200

    Add more dishes data to database

--- scripts/db.js
+++ scripts/db.js
@@ -1,26 +1,102 @@
 let myDishes = [
+  // Hauptgerichte
   {
-    id: 1,
+  id: 1,
     name: "Pizza Margherita",
     description: "Pizza mit Tomatensauce, Mozzarella und frischem Basilikum",
-    price: 8.99,
+    price: 8.9,
     image: "./assets/img/pizza-margherita.jpg",
     category: 'hauptgerichte'
   },
   {
-    id: 2,
+  id: 2,
     name: "Spaghetti Carbonara",
     description: "Pasta mit cremiger Sauce, Pancetta und Parmesan",
-    price: 10.99,
+    price: 10.9,
     image: "./assets/img/spaghetti-carbonara.jpg",
     category: 'hauptgerichte'
   },
   {
-    id: 3,
+  id: 3,
+    name: "Penne all'Arrabbiata",
+    description: "Pikante Tomatensauce mit Knoblauch und Chili",
+    price: 9.5,
+    image: "./assets/img/penne-arrabbiata.jpg",
+    category: 'hauptgerichte'
+  },
+  {
+  id: 4,
+    name: "Gegrillter Lachs",
+    description: "Lachsfilet mit Zitronen-Dill-Butter und Gemüsebeilage",
+    price: 14.9,
+    image: "./assets/img/lachs.jpg",
+    category: 'hauptgerichte'
+  },
+  // Beilagen
+  {
+  id: 5,
     name: "Caesar Salad",
     description: "Frischer Römersalat mit Caesar-Dressing und Croutons",
-    price: 7.99,
+    price: 7.9,
     image: "./assets/img/caesar-salad.jpg",
     category: 'beilagen'
+  },
+  {
+  id: 6,
+    name: "Bauernsalat",
+    description: "Frischer Salat mit Gurken, Tomaten, Paprika, Oliven und Feta",
+    price: 6.5,
+    image: "./assets/img/bauernsalat.jpg",
+    category: 'beilagen'
+  },
+  // Desserts
+  {
+  id: 7,
+    name: "Tiramisu",
+    description: "Klassisches italienisches Dessert mit Mascarpone und Espresso",
+    price: 5.5,
+    image: "./assets/img/tiramisu.jpg",
+    category: 'desserts'
+  },
+  {
+  id: 8,
+    name: "Schokoladenkuchen",
+    description: "Saftiger Kuchen mit dunkler Schokolade",
+    price: 4.9,
+    image: "./assets/img/chocolate-cake.jpg",
+    category: 'desserts'
+  },
+  // Getränke
+  {
+  id: 9,
+    name: "Spezi 0,33l",
+    description: "Mix aus Cola und Orange, gut gekühlt",
+    price: 2.7,
+    image: "./assets/img/spezi.jpg",
+    category: 'getraenke'
+  },
+  {
+  id: 10,
+    name: "Mineralwasser 0,5l",
+    description: "Still oder medium, frisch gekühlt",
+    price: 2.0,
+    image: "./assets/img/water.jpg",
+    category: 'getraenke'
+  },
+  {
+  id: 11,
+    name: "Espresso",
+    description: "Kräftiger kurzer Kaffee",
+    price: 2.2,
+    image: "./assets/img/espresso.jpg",
+    category: 'getraenke'
+  },
+  {
+  id: 12,
+    name: "Cappuccino",
+    description: "Espresso mit cremigem Milchschaum",
+    price: 3.2,
+    image: "./assets/img/cappuccino.jpg",
+    category: 'getraenke'
   }
 ];
